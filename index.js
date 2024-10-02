89d4dbac2bafed54a1fa2ed4a59e7197ea09eeda
Manikanta8887 <swamimanikanta66@gmail.com>
2024-10-02 15:54:47 +0530

    Adding Files

---
+++ index.js
@@ -0,0 +1,100 @@
+//  1st Question 
+
+var a="Manikanta";
+console.log(a);
+var a1=a.slice(2,7);
+console.log(a1);
+var a2=a1.toUpperCase();
+console.log(a2);
+
+//  2nd Question 
+
+var b="Manikanta";
+console.log(b);
+var b1=b.toUpperCase();
+console.log(b1);
+console.log(b.length);
+
+//  3rd Question 
+
+var c="Manikanta";
+console.log(c);
+var c1=c.charAt(4);
+console.log(c1);
+var c2=c.charCodeAt(4);
+console.log(c2);
+
+//  4th Question
+
+var d="    Manikanta    ";
+console.log(d);
+var d1=c.trim();
+console.log(d1);
+var d2=d1.slice(1,4);
+console.log(d2);
+
+//  5th Question 
+
+var m="Manikanta";
+console.log(m);
+var t="THIRNATI";
+console.log(t);
+var t1=t.toLowerCase();
+console.log(t1);
+var mt=m.concat(t1);
+console.log(mt);
+
+//  6th Question
+
+var e="    Manikanta    ";
+console.log(e);
+var e1=e.trim();
+console.log(e1);
+var e2=e1.slice(1);
+console.log(e2);
+
+//  7th Question
+
+var m1="Manikanta";
+console.log(m1);
+var m2=m1.charAt(4);
+console.log(m2);
+
+//  8th Question
+
+var t1="    Manikanta    ";
+console.log(t1);
+var t2=t1.trim();
+console.log(t2);
+var t3="Thirnati";
+console.log(t3);
+var t1t3=t2.concat(t3);
+console.log(t3);
+
+//  9th Question
+
+var mt1="    Manikanta    ";
+console.log(mt1);
+var mt2=mt1.trim();
+console.log(mt2);
+var mt3=mt2.toUpperCase();
+console.log(mt3);
+var mt4=mt3.slice(1,5);
+console.log(mt4);
+var mt5=mt4.toUpperCase();
+console.log(mt5);
+
+//  10th Question
+
+var s="Manikanta";
+console.log(s);
+var s1="Thirnati";
+console.log(s1);
+var s2=s.toLowerCase();
+console.log(s2);
+var s3=s1.toLowerCase();
+console.log(s3);
+var s5=s2.concat(s3);
+console.log(s5);
+var s6=s5.charAt(0);
+console.log(s6);
